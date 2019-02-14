ab215fb6c036da458c9a5c241dd98c9872381a93
9Jimmy <42477108+9Jimmy@users.noreply.github.com>
2019-02-14 16:01:35 +0500

    Update Notebook.java

--- src/main/java/com/domain/Notebook.java
+++ src/main/java/com/domain/Notebook.java
@@ -1,14 +1,13 @@
 package com.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+import javax.persistence.*;
 
 @Entity
+@Data
 public class Notebook {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
 
     private String tag;
@@ -19,30 +18,6 @@ public class Notebook {
 
     public Notebook(String tag, String text) {
         this.tag = tag;
-        this.text = text;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTag() {
-        return tag;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
         this.text = text;
     }
 }
