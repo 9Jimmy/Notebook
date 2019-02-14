dcf78dcb9d29b3ba9dfa991469aa425525fa1b85
9Jimmy <42477108+9Jimmy@users.noreply.github.com>
2019-02-14 16:04:52 +0500

    Update Notebook.java

--- src/main/java/com/domain/Notebook.java
+++ src/main/java/com/domain/Notebook.java
@@ -20,4 +20,28 @@ public class Notebook {
         this.tag = tag;
         this.text = text;
     }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
